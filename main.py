ff4eb9813b3bb1834554e9bd779133b4dbcd7b24
suki <sukidev07@gmail.com>
2025-10-01 22:25:55 -0400

    feature: added player.py, circleshape.py - objective to create a white triangle on steady in the middle of the screen.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # python
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
     # pygame.init()  <- Comment this out or delete it
     pygame.display.init() # <- Add this line
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()  # <- Keep this line
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2) # instantiate a player
     dt = 0  # <- Keep this line
     while True:
         for event in pygame.event.get():
@@ -14,10 +17,12 @@ def main():
                 return  # inside main()
 
         screen.fill("black")
-        pygame.display.flip()
+        player.draw(screen) # draw the player
+        pygame.display.flip() # update the display
 
-
+        # limit to 60 frames per second
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
+
 
 
 if __name__ == "__main__":
