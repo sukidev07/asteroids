69d254bedfe59503eef544aaa849d0d0c5430e5c
suki <sukidev07@gmail.com>
2025-10-01 21:32:32 -0400

    feature: added clock=pygame.time.Clock() and dt value of 0 then clock.tick(60) / 1000 to set limit to 60fps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     # pygame.init()  <- Comment this out or delete it
     pygame.display.init() # <- Add this line
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()  # <- Keep this line
+    dt = 0  # <- Keep this line
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +15,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
+
 
 if __name__ == "__main__":
     main()
